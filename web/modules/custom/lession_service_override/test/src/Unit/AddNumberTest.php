ee5d4c59a571fa1a17ec96424b63e905bac010f9
Mohammed Arshad <arshadkhan2631@gmail.com>
2021-08-21 15:17:17 +0530

    Added simple unit test.

---
+++ web/modules/custom/lession_service_override/test/src/Unit/AddNumberTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Template File Doc Comment
+ *
+ * PHP version 7
+ *
+ * @category Custom
+ * @package  Custom
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost:8080
+ */
+
+namespace Drupal\Tests\lesson_service_override\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Template File Doc Comment
+ *
+ * PHP version 7
+ *
+ * @category Custom
+ * @package  Custom
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost:8080
+ */
+
+class AddNumberTest extends UnitTestCase
+{
+
+    /**
+     * Tests a banned IP.
+     *
+     * @return mixed
+     */
+    public function testAddNumber()
+    {
+        $this->assertEquals((1 + 1), 2);
+    }
+
+}
